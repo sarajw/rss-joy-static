d4211311da28764f77e2895d852c73c4e90f072d
SJW <88535714+sarajw@users.noreply.github.com>
2024-02-04 15:07:42 +0100

    some tidying

---
+++ src/data/feeds.js
@@ -0,0 +1,14 @@
+export const feedURLs = [
+	"https://sarajoy.dev/rss.xml",
+	"https://localghost.dev/feed.xml",
+	"https://www.htmhell.dev/feed.xml",
+	"https://chriskirknielsen.com/rss.xml",
+	"https://rknight.me/subscribe/posts/rss.xml",
+	"https://mxb.dev/feed.xml",
+	"https://matthiasott.com/feed",
+	"https://buttondown.email/ownyourweb/rss",
+	"https://noahliebman.net/feed/index.xml",
+	"https://chriscoyier.net/feed/",
+	"https://daverupert.com/atom.xml",
+	"https://www.mayank.co/blog/rss.xml",
+];
