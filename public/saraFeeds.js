1912cb10b0c73964e71c0f9bcc695c5df925be6b
SJW <88535714+sarajw@users.noreply.github.com>
2024-03-03 23:11:27 +0100

    xkcd dodgy pubDate?

--- public/saraFeeds.js
+++ public/saraFeeds.js
@@ -1,6 +1,6 @@
 export const feedURLs = [
 	{ "url": "https://sarajoy.dev/rss.xml" },
-  { "url": "https://xkcd.com/rss.xml" },
+  // { "url": "https://xkcd.com/rss.xml" },
   { "url": "https://waitbutwhy.com/feed" },
   { "url": "https://www.theguardian.com/tone/cartoons/rss" },
   { "url": "https://sidebar.io/feed.xml" },
