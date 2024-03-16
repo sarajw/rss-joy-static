cfb01657fcf1007a87527833132bc3d0f737b053
Sara Joy <88535714+sarajw@users.noreply.github.com>
2024-03-17 00:27:08 +0100

    Update saraFeeds.js

--- public/saraFeeds.js
+++ public/saraFeeds.js
@@ -1,7 +1,7 @@
 export const feedURLs = [
-	{ "url": "https://sarajoy.dev/rss.xml" },
+	// { "url": "https://sarajoy.dev/rss.xml" },
   // { "url": "https://xkcd.com/rss.xml" },
-  { "url": "https://waitbutwhy.com/feed" },
-  { "url": "https://www.theguardian.com/tone/cartoons/rss" },
-  { "url": "https://sidebar.io/feed.xml" },
+ //  { "url": "https://waitbutwhy.com/feed" },
+//   { "url": "https://www.theguardian.com/tone/cartoons/rss" },
+//  { "url": "https://sidebar.io/feed.xml" },
 ];
