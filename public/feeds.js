51f144439818ca0cecf3c07327f12fa5de43bbc2
Sara Joy <88535714+sarajw@users.noreply.github.com>
2024-04-20 21:12:35 +0200

    Update feeds.js

--- public/feeds.js
+++ public/feeds.js
@@ -16,7 +16,7 @@ export const feedURLs = [
 	{ "url": "https://saneef.com/feed.xml" },
 	{ "url": "https://dutchcelt.nl/feed.xml", "order": "reverse" },
 	{ "url": "https://kilianvalkhof.com/feed/" },
-	{ "url": "https://robinrendle.com/cascadefeed.xml" },
+	{ "url": "https://www.csscade.com/feed.rss" },
 	{ "url": "https://robinrendle.com/feed.xml" },
 	{ "url": "https://gkeenan.co/avgb?format=rss", "posts": 3 },
 	{ "url": "https://lea.codes/rss.xml"},
