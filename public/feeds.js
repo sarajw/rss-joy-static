bffb177d738567063ba3b97f69d72037a03a56e6
Sara Joy <88535714+sarajw@users.noreply.github.com>
2024-05-16 13:25:39 +0200

    reverse a feed

--- public/feeds.js
+++ public/feeds.js
@@ -26,7 +26,7 @@ export const feedURLs = [
 	{ "url": "https://www.stefanjudis.com/rss.xml" },
 	{ "url": "https://ohhelloana.blog/feed.xml" },
 	{ "url": "https://shkspr.mobi/blog/feed/atom/", "posts": 4 },
-	{ "url": "https://thomasrigby.com/feed.xml" },
+	{ "url": "https://thomasrigby.com/feed.xml", "order": "reverse" },
 	{ "url": "https://briefs.video/feed.xml", "order": "reverse" },
 	{ "url": "https://heydonworks.com/feed.xml" },
 	{ "url": "https://joshcollinsworth.com/api/rss.xml", "posts": 4 },
