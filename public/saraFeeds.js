4d19497e9d8dcab12665d2ee59f6557680e12cce
SJW <88535714+sarajw@users.noreply.github.com>
2024-12-10 22:48:03 +0100

    delete dodgy pages?

--- public/saraFeeds.js
+++
@@ -1,7 +0,0 @@
-export const feedURLs = [
-	{ "url": "https://sarajoy.dev/rss.xml" },
-	// { "url": "https://xkcd.com/rss.xml" }, //dodgy pubDate
-	{ "url": "https://waitbutwhy.com/feed" },
-	{ "url": "https://www.theguardian.com/tone/cartoons/rss" },
-	{ "url": "https://chriscoyier.net/feed/" },
-];
